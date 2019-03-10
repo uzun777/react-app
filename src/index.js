8cbccfc20bf418b7adf5b65a233957d7addb75a8
uzun777 <33759347+uzun777@users.noreply.github.com>
2019-03-10 23:02:52 +0200

    element added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// serviceWorker.unregister();
 
 //ReactDOM.render(<h1>Hello World</h1>,document.getElementById("root"));
 
+function tick(params) {
+    let element=(
+        <div>
+        <h1>Hello</h1>
+        <h2>Time is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    ReactDOM.render(element,document.getElementById('root'))
+};
+
+setInterval(tick,1000);
